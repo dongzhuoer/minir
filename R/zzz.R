3ac38b7926b3e090e581aeefc4af55046cb8ac5d
Zhuoer Dong <dongzhuoer@mail.nankai.edu.cn>
2018-11-01 17:05:25 +0800

    update package style

--- R/zzz.R
+++ R/zzz.R
@@ -1,14 +1,7 @@
 .onAttach <- function(libname, pkgname) {
-    # packageStartupMessage("Welcome Zhuoer Dong's little package")
+    # packageStartupMessage("Welcome to minir")
 }
 
 .onLoad <- function(libname, pkgname) {
-    # minir.options <- list(
-    #   minir.foo = "bar"
-    # )
-
-    # to_set <- !(names(minir.options) %in% names(options()))
-    # if(any(to_set)) options(minir.options[to_set])
-
-    invisible()
+    
 }
--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -1,7 +1,4 @@
 testthat::context('Testing data')
-if (basename(getwd()) == 'testthat') setwd('../..')
+if (basename(getwd()) == 'testthat') setwd('../..')  # workspace is reset per file
 
-testthat::test_that('sample_100', {
-    testthat::expect_identical(length(sample_100), 100L);
-    testthat::expect_true(sum(sample_100) == 5050);
-});
+
